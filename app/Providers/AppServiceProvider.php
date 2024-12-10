985da1b445b700ae539eab71bed85e9436b9a42a
Christoph Rumpel <christoph@christoph-rumpel.com>
2024-12-10 12:05:20 +0100

    Step 9 - Solution binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\AiService;
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(AiService::class, function () {
+            return new AiService(new Client(), [config('services.ai.key')]);
+        });
     }
 
     /**
